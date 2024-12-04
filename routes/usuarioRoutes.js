d0bbd39c8a0e5b4f3fa681f58ded6a7063265113
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-03 23:47:20 -0300

    Optimizando routing con .router para / metodos get y post

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,11 +3,7 @@ import express from "express"
 const router = express.Router()
 
 // Routing
-router.get('/', function(req, res) {
-	res.send('Hola Mundo en Express')
-})
-
-router.get('/nosotros', function(req, res) {
-	res.json({msg: 'Hola Mundo'})
-})
+router.route('/')
+	.get((req, res) => res.send('Hola Mundo desde Express'))
+	.post((req, res) => res.json({msg: 'Método Post'}))
 export default router
