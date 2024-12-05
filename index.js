31e2d280978e8952fe29cbf61092c59e43924d37
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-04 22:41:10 -0300

    Conectando la app a mysql con mysql2 y sequelize como ORM

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from 'express'
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import db from './config/db.js'
+
 const app = express()
+
+// Conexión a la base de datos
+try {
+    await db.authenticate()
+    console.log('Conexión correcta a la base de datos')
+} catch(error) {
+    console.log(error);
+    
+}
 
 // Habilitar template engine (Pug)
 app.set('view engine', 'pug')
