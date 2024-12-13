968d801186a0ebe4c6baea57d363192d7f48b89f
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-13 17:20:03 -0300

    Comenzando el formulario de crear propiedades

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -5,6 +5,15 @@ const admin = (req, res) => {
     })
 }
 
+// Formulario para crear una nueva propiedad
+const crear = (req, res) => {
+    res.render('propiedades/crear', {
+        titulo: 'Crear Propiedad',
+        barra: true
+    })
+}
+
 export {
-    admin
+    admin,
+    crear
 }
