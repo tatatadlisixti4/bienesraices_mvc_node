73644f9d1608b9b75bbffda6176e08bd47c849bb
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-16 14:10:05 -0300

    Ajustes modelo propiedad

--- models/Propiedad.js
+++ models/Propiedad.js
@@ -1,2 +1,19 @@
 import {DataTypes} from 'sequelize'
 import db from '../config/db.js'
+
+const Propiedad = db.define('propiedades', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    titulo: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    descripcion: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+})
