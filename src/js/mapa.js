44cdb01c21e051735504cfd6f04ae74795b1ebb9
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-15 23:31:24 -0300

    Obtener la latitud y longitud del pin

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -3,7 +3,7 @@
     const lat = 40.4530822
     const lng = -3.6903137
     const mapa = L.map('mapa').setView([lat, lng ], 17)
-    let marke
+    let marker
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -14,7 +14,12 @@
         draggable: true, // Se puede mover el pin
         autoPan: true // Una vez movido el pin, se vuelve a centrar el mapa
     })
-    .addTo(mapa)
+    .addTo(mapa) // Añade a la instancia de marker 
 
-
+    // Detectar el movimiento del pin
+    marker.on('moveend', function(e){ // Manejo evento que provee leaflet
+        marker = e.target 
+        const posicion = marker.getLatLng() // Metodo para extraer longitu y latitud
+        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng)) // Toma el mapa y lo centra en funcipon a la posición del pin
+    })
 })()
