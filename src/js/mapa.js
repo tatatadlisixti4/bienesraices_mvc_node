df395696e17b197829c0fb60d993639916c0afd0
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-16 00:19:07 -0300

    Almacenando latitud, longitud y calle

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -31,6 +31,11 @@
             console.log(resultado) // Objeto con información de posición
             marker.bindPopup(resultado.address.LongLabel)
             
+            // Llenar los campos
+            document.querySelector('.calle').textContent = resultado?.address?.Address ?? '';
+            document.querySelector('#calle').value = resultado?.address?.Address ?? '';
+            document.querySelector('#lat').value = resultado?.latlng?.lat ?? '';
+            document.querySelector('#lng').value = resultado?.latlng?.lng ?? '';
         })
     })
 })()
