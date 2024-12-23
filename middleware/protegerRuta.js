4c7d70a7c8ceca584ca6261d7c38da7cf84321f1
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-23 00:00:28 -0300

    Leyendo y validando el JWT

--- middleware/protegerRuta.js
+++ middleware/protegerRuta.js
@@ -1,8 +1,22 @@
+import jwt from 'jsonwebtoken'
+import {Usuario} from '../models/index.js'
+
 const protegerRuta = async (req, res, next) => {
     // Verificar si hay un token
+    const {_token} = req.cookies
+    if(!_token) {
+        return res.redirect('/auth/login')
+    }
 
     // Comprobar el token
-
+    try {
+        const decoded = jwt.verify(_token, process.env.JWT_SECRET)
+        const usuario = await Usuario.findByPk(decoded.id)
+        console.log(usuario)
+        
+    } catch (error) {
+        console.log(error)
+    }
     next()
 }
 
