4b72865d661bf74e8429ba00bc33abd9ec8de40d
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-22 23:27:52 -0300

    Creando un custom middleware para validar rutas

---
+++ middleware/protegerRuta.js
@@ -0,0 +1,9 @@
+const protegerRuta = async (req, res, next) => {
+    // Verificar si hay un token
+
+    // Comprobar el token
+
+    next()
+}
+
+export default protegerRuta
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import {body} from 'express-validator'
 import {admin, crear, guardar} from '../controllers/propiedadController.js'
+import protegerRuta from '../middleware/protegerRuta.js'
 
 const router = express.Router()
 
-router.get('/mis-propiedades', admin)
+router.get('/mis-propiedades', protegerRuta, admin)
 router.get('/propiedades/crear', crear)
+
 router.post('/propiedades/crear', 
     body('titulo')
         .notEmpty()
